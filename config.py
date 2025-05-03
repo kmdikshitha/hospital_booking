ebaec0f1e8d4b3bfa1679baab538b9c248dd0166
jahnavi-vemuri <jvemuri@iu.edu>
2025-05-03 12:38:15 -0400

    Updated database URI for production

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'your-secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'hospital.db')
+    
+    # Check if we are in a production environment or not
+    if os.getenv('FLASK_ENV') == 'production':
+        # Use Render's persistent storage for SQLite
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///mnt/data/hospital.db'
+    else:
+        # Use the local directory for SQLite when developing locally
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'hospital.db')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False

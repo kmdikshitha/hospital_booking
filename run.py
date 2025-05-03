24040522dd20885237c014fff6a3e45c4f2cf292
jahnavi-vemuri <jvemuri@iu.edu>
2025-05-03 13:42:00 -0400

    modified run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from app import create_app
 from app.models import db
-from app.utils.data_loader import load_csv_data
+from app.data_loader import load_all
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-    load_csv_data()  # load once
+    load_all()  # load once
 
 if __name__ == "__main__":
     app.run(debug=True)
